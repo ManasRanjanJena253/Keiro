034e519920f61798c51e198a384d9e30145724ff
Manas Ranjan Jena <mranjanjena253@gmail.com>
2026-05-28 10:43:26 +0530

    Added Embedding cache

---
+++ gateway/cache/embedding_cache.go
@@ -0,0 +1,36 @@
+package cache
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+type EmbeddingCache struct {
+	lruStore *LRUStore
+}
+
+func NewEmbeddingCache(store *LRUStore) *EmbeddingCache {
+	return &EmbeddingCache{lruStore: store}
+}
+
+func (embedCache *EmbeddingCache) Set(query string, embeddings []float32) {
+	hashedKey := sha256.Sum256([]byte(query))
+	encodedKey := hex.EncodeToString(hashedKey[:])
+	embedCache.lruStore.Set(encodedKey, embeddings)
+}
+
+func (embedCache *EmbeddingCache) Get(key string) ([]float32, bool) {
+	hashedKey := sha256.Sum256([]byte(key))
+	keyStr := hex.EncodeToString(hashedKey[:])
+
+	val, ok := embedCache.lruStore.Get(keyStr)
+	if !ok {
+		return nil, false
+	}
+
+	vec, ok := val.([]float32)
+	if !ok {
+		return nil, false
+	}
+	return vec, true
+}

17be2c1a0557806d9f1ddbd38473fc0d56ee4e04
Manas Ranjan Jena <mranjanjena253@gmail.com>
2026-05-30 09:57:56 +0530

    Added the job tracker

---
+++ gateway/queue/job_tracker.go
@@ -0,0 +1,62 @@
+package queue
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+type status int
+
+const (
+	Pending = iota
+	Processing
+	Completed
+	Failed
+)
+
+type jobEntry struct {
+	jobStatus status
+	jobError  string
+}
+
+type JobTracker struct {
+	jobsMap map[uuid.UUID]*jobEntry
+	mutex   *sync.RWMutex
+}
+
+func NewJobTracker() *JobTracker {
+	jobMap := make(map[uuid.UUID]*jobEntry)
+	mutex := sync.RWMutex{}
+	return &JobTracker{jobsMap: jobMap, mutex: &mutex}
+}
+
+func (tracker *JobTracker) CreateJob() (uuid.UUID, error) {
+	tracker.mutex.Lock()
+	defer tracker.mutex.Unlock()
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	tracker.jobsMap[id] = &jobEntry{jobError: "", jobStatus: Pending}
+	return id, nil
+}
+
+func (tracker *JobTracker) UpdateStatus(id uuid.UUID, stat status, errMsg string) {
+	tracker.mutex.Lock()
+	defer tracker.mutex.Unlock()
+	job := tracker.jobsMap[id]
+	job.jobStatus = stat
+	job.jobError = errMsg
+}
+
+func (tracker *JobTracker) GetStatus(id uuid.UUID) (*jobEntry, error) {
+	tracker.mutex.RLock()
+	defer tracker.mutex.RUnlock()
+	job := tracker.jobsMap[id]
+	if job == nil {
+		return nil, errors.New("job not found")
+	}
+	return job, nil
+}

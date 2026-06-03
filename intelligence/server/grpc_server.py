7feeea41f10182d1ecca8cc2d773c72a38c55345
Manas Ranjan Jena <mranjanjena253@gmail.com>
2026-06-03 14:27:32 +0530

    Added the Execute Retrieval grpc

--- intelligence/server/grpc_server.py
+++ intelligence/server/grpc_server.py
@@ -2,11 +2,16 @@ import sys
 import os
 from dotenv import load_dotenv
 from google import genai
+from sentence_transformers import CrossEncoder
 
 from embeddings.base_embedder import BaseEmbedder
 from embeddings.local_embedder import LocalEmbedder
 from ingestion.chunker import Chunker
 from ingestion.pipeline import IngestionPipeline
+from reranker.cross_encoder_reranker import CrossEncoderReranker
+from retrieval.complex_retriever import ComplexRetriever
+from retrieval.multihop_retriever import MultiHopRetriever
+from retrieval.simple_retriever import SimpleRetriever
 from vectorstore.chroma_store import ChromaStore
 from classifier.query_classifier import ClassifyQuery
 from classifier.strategy_selector import get_config
@@ -17,9 +22,12 @@ import rag_pb2_grpc
 from concurrent import futures
 
 class IntelligenceServiceServicer(rag_pb2_grpc.IntelligenceServiceServicer):
-    def __init__(self, pipeline: IngestionPipeline, classifier: ClassifyQuery):
+    def __init__(self, pipeline: IngestionPipeline, classifier: ClassifyQuery, simple_retriever: SimpleRetriever, complex_retrievr: ComplexRetriever, multi_hop_retriever: MultiHopRetriever):
         self.pipeline = pipeline
         self.classifier = classifier
+        self.simple = simple_retriever
+        self.complex = complex_retrievr
+        self.multi_hop = multi_hop_retriever
 
     def ComputeEmbeddings(self, request, context):
         return super().ComputeEmbeddings(request, context)
@@ -48,7 +56,49 @@ class IntelligenceServiceServicer(rag_pb2_grpc.IntelligenceServiceServicer):
             return rag_pb2.ClassifyQueryResponse()
 
     def ExecuteRetrieval(self, request, context):
-        return super().ExecuteRetrieval(request = request, context = context)
+        config = request.received_config
+        query = request.user_query
+        namespace = request.namespace
+
+        top_k = config.top_k
+        retrieval_type = config.retrieval_type
+
+        try:
+            if retrieval_type in (rag_pb2.RetrievalType.HYBRID, rag_pb2.RetrievalType. RETRIEVAL_TYPE_UNSPECIFIED):
+                chunks = self.simple.retrieve_top_k(namespace, top_k, query)
+
+            elif retrieval_type == rag_pb2.RetrievalType.MULTI_VECTOR:
+                chunks = self.complex.retrieve_top_k(namespace, top_k, query)
+
+            elif retrieval_type == rag_pb2.RetrievalType.SELF_QUERYING:
+                chunks = self.multi_hop.retrieve_top_k(namespace, top_k, query)
+
+            else:
+                context.set_code(grpc.StatusCode.INTERNAL)
+                context.set_details("Unknown retrieval type found")
+                return rag_pb2.ExecuteRetrievalResponse(
+                    retrieval_status = False
+                )
+
+            retrieved_chunks = [rag_pb2.RetrievedChunk(
+                text = chunk,
+                metadata = {},
+                similarity_score = 0.0,
+                source_id = ""
+            ) for chunk in chunks
+            ]
+
+            return rag_pb2.ExecuteRetrievalResponse(
+                retrieved_chunk = retrieved_chunks,
+                retrieval_status = True
+            )
+
+        except ValueError as e:
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(str(e))
+            return rag_pb2.ExecuteRetrievalResponse(
+                retrieval_status = False
+            )
 
     def GenerateResponse(self, request, context):
         return super().GenerateResponse(request = request, context = context)
@@ -115,7 +165,15 @@ def serve():
 
     classifier = ClassifyQuery(client = client, model_name = model)
 
-    rag_pb2_grpc.add_IntelligenceServiceServicer_to_server(IntelligenceServiceServicer(pipeline = pipeline, classifier = classifier), server)
+    simple_retriever = SimpleRetriever(store = store, embedder = embedder)
+
+    cross_encoder_model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L6-v2")
+    cross_encoder = CrossEncoderReranker(cross_encoding_model = cross_encoder_model)
+    complex_retriever = ComplexRetriever(store = store, embedder = embedder, client = client, cross_encoder = cross_encoder, model_name = model, mmr_lambda = float(os.getenv("KEIRO_MMR_RETRIEVAL_LAMBDA")))
+
+    multi_hop_retriever = MultiHopRetriever(embedder = embedder, store = store, client = client, model_name = model, num_hops = 3)
+
+    rag_pb2_grpc.add_IntelligenceServiceServicer_to_server(IntelligenceServiceServicer(pipeline = pipeline, classifier = classifier, simple_retriever = simple_retriever, complex_retriever = complex_retriever, multi_hop_retriever = multi_hop_retriever), server)
     server.add_insecure_port(f"{HOST}:{PORT}")
     print(f"Starting the server at port {HOST}:{PORT}")
     server.start()
